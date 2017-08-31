76b78477766ebd5db6cb0e9330d3fef1f02b90bf
jxr5398 <jxr5398@WGATEE206-81.win.psu.edu>
2017-08-31 12:56:11 -0400

    Navigation

--- IST361Round1/src/ist361round1/NavigationView.java
+++ IST361Round1/src/ist361round1/NavigationView.java
@@ -5,36 +5,15 @@
  */
 package ist361round1;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+
 /**
  *
  * @author lap5508
  */
-public class NavigationView {
+public class NavigationView extends JFrame {
     
     public NavigationView(NavigationController parentNavCntl){
-        /*
-        theNavCntl = parentNavCntl;
-        createLabelPanel();
-        createNavigationPanel();
-        createClosePanel();
-        setBounds(0, 0, 650, 650);
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        */
+       
     }
 }
