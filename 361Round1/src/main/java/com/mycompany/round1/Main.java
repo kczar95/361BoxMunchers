65a1cebffd2f6cee9bcee72f2133942d55912918
cmg5831 <cmg5831@WGATEE206-85.win.psu.edu>
2017-09-05 12:06:14 -0400

    Updated package

--- 361Round1/src/main/java/com/mycompany/round1/Main.java
+++ 361Round1/src/main/java/com/mycompany/round1/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ist361round1;
+package com.mycompany.round1;
 import javax.swing.*;
 /**
  *
--- 361Round1/src/main/java/com/mycompany/round1/NavigationView.java
+++ 361Round1/src/main/java/com/mycompany/round1/NavigationView.java
@@ -4,7 +4,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ist361round1;
+package com.mycompany.round1;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
