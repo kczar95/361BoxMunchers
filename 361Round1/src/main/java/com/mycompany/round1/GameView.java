aacbe0f571c08b24c3a8ca5615ed7d1cf1223860
jxr5398 <jxr5398@WGATEE206-81.win.psu.edu>
2017-09-07 13:23:25 -0400

    Fix some errors and clean up GameController

--- 361Round1/src/main/java/com/mycompany/round1/GameView.java
+++ 361Round1/src/main/java/com/mycompany/round1/GameView.java
@@ -17,11 +17,10 @@ import javax.swing.JSpinner;
 import javax.swing.SpinnerListModel;
 import javax.swing.SwingConstants;
 
-
 public final class GameView extends JFrame {
-    
+
     private NavigationController nControl = null;
-    
+
     //info panel components
     private JPanel infoPanel;
     private JLabel scoreLabel;
@@ -33,96 +32,91 @@ public final class GameView extends JFrame {
     private JLabel gameLabel;
     private JButton leaderboardButton;
     GridLayout infoGrid = new GridLayout(2, 3);
-    
-    
+
     //game board components
     private JPanel gamePanel;
     private JButton boardPiece;
     GridLayout boardGrid = new GridLayout(9, 9);
     JButton[][] gameBoard = new JButton[9][9];
-    
+
     //game board data
     private int[][] gameData;
 
     public GameView(NavigationController n_control) {
-        nControl = n_control;       
+        nControl = n_control;
+        multipleOf_Spinner = new JSpinner();
         createInfoPanel();
         createGameBoardPanel(gameData);
         this.setVisible(true);
         setBounds(0, 0, 650, 650);
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
     }
-    
-    public void createInfoPanel(){
+
+    public void createInfoPanel() {
         infoPanel = new JPanel();
-        GridLayout infoGrid = new GridLayout(2,3, 0, 50);
+        GridLayout infoGrid = new GridLayout(2, 3, 0, 50);
         infoPanel.setLayout(infoGrid);
         playButton = new JButton("Play");
-            //
-            //
+        //
+        //
         pauseButton = new JButton("Pause");
-            //
-            //
+        //
+        //
         gameLabel = new JLabel("Multiple Of:", SwingConstants.CENTER);
-            //
-            //
+        //
+        //
         //multipleOf_Spinner = new JSpinner("Multiple of:");
-            //
-            //
+        //
+        //
         leaderboardButton = new JButton("Leaderboard");
-            leaderboardButton.addActionListener((java.awt.event.ActionEvent evt) -> {
-                leaderboardButtonActionPerformed(evt);
-                }
+        leaderboardButton.addActionListener((java.awt.event.ActionEvent evt) -> {
+            leaderboardButtonActionPerformed(evt);
         });
-            //
+        //
         scoreLabel = new JLabel("Score:", SwingConstants.CENTER);
-            //
-            //
+        //
+        //
         infoPanel.add(playButton);
         infoPanel.add(pauseButton);
         infoPanel.add(leaderboardButton);
         infoPanel.add(scoreLabel);
         infoPanel.add(gameLabel);
         infoPanel.add(multipleOf_Spinner);
-        add(infoPanel, BorderLayout.NORTH);       
+        add(infoPanel, BorderLayout.NORTH);
     }
-    
+
     //Creating the values for each button
-    public int[][] createGameData(){
+    public int[][] createGameData() {
         int[][] tempData = new int[9][9];
-        for(int i=0;i<tempData.length;i++){
-            for(int j=0;j<tempData.length;j++){
-                if(tempData[i][j] == 0){
+        for (int i = 0; i < tempData.length; i++) {
+            for (int j = 0; j < tempData.length; j++) {
+                if (tempData[i][j] == 0) {
                     tempData[i][j] = (int) (Math.random() * 100);
                 }
             }
         }
-        return tempData; 
+        return tempData;
     }
-    
-    
+
     public void createGameBoardPanel(int[][] gameData) {
         gamePanel = new JPanel();
         add(gamePanel);
-        GridLayout grid = new GridLayout(9,9);
+        GridLayout grid = new GridLayout(9, 9);
         gameBoard = new JButton[9][9];
         gamePanel.setLayout(grid);
         JButton[][] boardSquares = new JButton[9][9];
-        for (int i=0; i<9;i++)
-        {
-            for(int j=0; j<9;j++)
-            {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 //Create buttons and add their values
-                if(gameBoard[i][j] == null )
-                {
+                if (gameBoard[i][j] == null) {
                     boardSquares[i][j] = new JButton(Integer.toString(gameData[i][j]));
                     gamePanel.add(boardSquares[i][j]);
                 }
             }
         }
     }
-    
-    private void leaderboardButtonActionPerformed(ActionEvent evt){
+
+    private void leaderboardButtonActionPerformed(ActionEvent evt) {
         GameView.this.nControl.showLeaderBoard();
     }
 }
